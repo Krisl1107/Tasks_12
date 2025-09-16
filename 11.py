b83e6b7084d390cdffebff06550a41ed89778021
Krisl1107 <Klagoda_zeya@mail.ru>
2025-09-16 11:30:55 +0700

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,8 +1,22 @@
+def letters(x,y):
+    if x[-1]=="ь" or x[-1]=="ы" :
+        if x[-2]==y[0]:
+            return True
+        else:
+            return False
+
+def right(x,y):
+    if x[-1] == y[0].lower():
+        return True
+    else:
+        return False
+
+
 text=input()
 words=text.split()
 win=""
 for i in range(0,len(words)-1):
-    if (words[i])[-1]== ((words[i+1])[0]).lower():
+    if (right(words[i],words[i+1])) and (letters(words[i],words[i+1].lower())):
         continue
     else:
         if i%2==0:
@@ -10,6 +24,7 @@ for i in range(0,len(words)-1):
         else:
             win = "Победил Вася"
         break
+
 
 if win=="":
  if len(words)%2==0:
